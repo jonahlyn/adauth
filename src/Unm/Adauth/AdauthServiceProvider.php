4d5789eaf48de956f825a3907c8f51d84890938e
Jonahlyn Gilstrap <jgilstrap@gmail.com>
2014-01-22 15:50:08 -0700

    Whitespace issues.

--- src/Unm/Adauth/AdauthServiceProvider.php
+++ src/Unm/Adauth/AdauthServiceProvider.php
@@ -9,25 +9,24 @@ use Illuminate\Support\ServiceProvider;
 class AdauthServiceProvider extends ServiceProvider
 {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+  /**
+   * Indicates if loading of the provider is deferred.
+   *
+   * @var bool
+   */
+  protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('unm/adauth');
+  /**
+   * Bootstrap the application events.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    $this->package('unm/adauth');
 
     //Add the AD Auth driver
-    $this->app['auth']->extend('adauth', function($app)
-    {
+    $this->app['auth']->extend('adauth', function ($app) {
       return new AdauthGuard(
         new AdAuthUserProvider(
           $app['config']->get('adauth::adauth')
@@ -35,26 +34,26 @@ class AdauthServiceProvider extends ServiceProvider
         $app->make('session.store')
       );
     });
-	}
+  }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
+  /**
+   * Register the service provider.
+   *
+   * @return void
+   */
+  public function register()
+  {
 
-	}
+  }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('adauth');
-	}
+  /**
+   * Get the services provided by the provider.
+   *
+   * @return array
+   */
+  public function provides()
+  {
+    return array('adauth');
+  }
 
 }
